4a4ff2206be37e74c5585efb0c8260390e0f5af5
jean-baptiste faria <fariajeanbap@gmail.com>
2023-05-18 14:13:43 +0200

    resolution du probleme de route sign out devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'static_pages#index'
+  devise_for :users
 
-  # Defines the root path route ("/")
+  # Ajoutez cette ligne pour la déconnexion des utilisateurs
+  devise_scope :user do
+    get 'users/sign_out', to: 'devise/sessions#destroy'
+  end
+
+  get 'secret', to: 'static_pages#secret'
+
+  # Définissez vos routes d'application selon la DSL dans https://guides.rubyonrails.org/routing.html
+
+  # Définit la route pour la racine ("/")
   # root "articles#index"
 end
