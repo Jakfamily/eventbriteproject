dc294f4130ce897429921a4413fc9d626c8bf224
jean-baptiste faria <fariajeanbap@gmail.com>
2023-05-18 22:34:38 +0200

    mise en forme des events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name, :last_name, :description, :email, :password, :password_confirmation)}
     devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name, :last_name, :description, :email, :password, :password_confirmation)}
   end
+
+  def user_first_name
+    first_name
+  end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def index
+    @events = Event.all
   end
 
   def secret
