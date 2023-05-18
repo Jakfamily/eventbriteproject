85329b9b2514237a64fe38af35202c724d86c3ff
jean-baptiste faria <fariajeanbap@gmail.com>
2023-05-18 14:49:29 +0200

    mise en pages des form sign up sign in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,12 @@
 class StaticPagesController < ApplicationController
-
-
   def index
   end
 
   def secret
+    if user_signed_in?
+      # Code pour la page secret accessible uniquement aux utilisateurs connectés
+    else
+      redirect_to new_user_session_path
+    end
   end
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
-  devise_for :users
 
-  # Ajoutez cette ligne pour la déconnexion des utilisateurs
+  # Déconnexion des utilisateurs
   devise_scope :user do
     get 'users/sign_out', to: 'devise/sessions#destroy'
   end
 
-  get 'secret', to: 'static_pages#secret'
+  devise_for :users, controllers: {
+    sessions: 'devise/sessions',
+    registrations: 'devise/registrations'
+  }
 
-  # Définissez vos routes d'application selon la DSL dans https://guides.rubyonrails.org/routing.html
+  # Redirection après connexion
+  authenticated :user do
+    root 'static_pages#secret', as: :authenticated_root
+  end
 
-  # Définit la route pour la racine ("/")
-  # root "articles#index"
+  unauthenticated do
+    root 'static_pages#index', as: :unauthenticated_root
+  end
 end
