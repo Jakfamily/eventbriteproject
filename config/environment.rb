afdde555fe91d4ef727209582241a86ccf2b7f5c
jean-baptiste faria <fariajeanbap@gmail.com>
2023-05-19 12:38:55 +0200

    correction du bug daffaichage

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,12 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.default_options = { from: 'eventbrite@example.com' }
+ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'heroku.com',
+    :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
