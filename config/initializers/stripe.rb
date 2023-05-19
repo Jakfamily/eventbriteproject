c6b4f23cde5099ac92fe677ebbbfef1b7754a5e2
jean-baptiste faria <fariajeanbap@gmail.com>
2023-05-19 21:06:50 +0200

    ajout de stripe v2

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,8 @@
-require 'dotenv'
+require 'dotenv' Dotenv.load('.env')
+
 
 Rails.configuration.stripe = {
-    publishable_key:'pk_test_51N9WNvKEgZiwtZMaFKqMHEyc3ZbjMCqmfQCoc4Hlh4j8QIR2Po4TYJCwvc9nmCJxveEqztG8XohpTVnxkNH8g1yE00E2Jcdwyr',
-    secret_key:'sk_test_51N9WNvKEgZiwtZMaP8UWH4RzwJsVBCp0O6rRlzsDQNpT4hotMvaKmdD73tz2xM3uAaDG1mK5DN25KtJ2db8SF5ys00whcb0xZp'
-  }
-  Stripe.api_key = Rails.configuration.stripe[:secret_key]
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :secret_key      => ENV['SECRET_KEY']
+}Stripe.api_key = Rails.configuration.stripe[:secret_key]
   
